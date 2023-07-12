da5a753c987f3d05fa5158613472b236353e4364
Poltavskay <poltavskay9@gmail.com>
2023-07-12 16:13:13 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react"
 
 
 export default function App() {
-  return <div className="App">Weather App </div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Galina Poltavskay and is{" "}
+        <a
+          href="https://github.com/Poltavskay/react-site"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
