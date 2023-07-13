6b5cab1712fa4df467d19b24228ad20dd203dd6d
Poltavskay <poltavskay9@gmail.com>
2023-07-13 10:43:35 +0300

    Added bootstap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import React from "react"
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Galina Poltavskay and is{" "}
-        <a
-          href="https://github.com/Poltavskay/react-site"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div class="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Galina Poltavskay and is{" "}
+          <a
+            href="https://github.com/Poltavskay/react-site"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
