6864233e7f6477a27a308ed9019c0f981d0d2bc7
Poltavskay <poltavskay9@gmail.com>
2023-07-15 10:10:43 +0300

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react"
-
+import Weather from "./Weather"
 
 export default function App() {
   return (
     <div className="App">
       <div class="container">
-        <h1> Weather App</h1>
+        <Weather />
         <footer>
           This project was coded by Galina Poltavskay and is{" "}
           <a
